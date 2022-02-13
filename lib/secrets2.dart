32bc5f68f4c44f80b4d616309595f4ce7eece38b
taylorngo <cs.taylorngo@gmail.com>
2022-02-13 04:17:38 -0500

    added secrets2.dart for clarification

---
+++ lib/secrets2.dart
@@ -0,0 +1,11 @@
+/// RENAME TO secrets.dart and include all api keys here:
+/// Google API Key
+/// Twilio Account SID
+/// Twilio Auth Token
+/// Twilio Phone Number
+/// Personal Phone Number
+String googleApiKey = '';
+String accountSid = '';
+String authToken = '';
+String twilioNumber = '';
+String phoneNumber = '';
